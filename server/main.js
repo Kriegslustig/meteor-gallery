57fe21c5099fe06c04676a844d9e45f60c48d030
Kriegslustig <github@ls7.ch>
2015-01-28 13:35:49 +0100

    Read the right vars from the gallerz template and croping images

--- server/main.js
+++ server/main.js
@@ -8,8 +8,15 @@ makeThumbNail = function (fileBuffer, extenstion, callback) {
     gmRessource.size(callback)
   }
   var imageSize = Meteor.wrapAsync(syncSize)(gmImageRessource)
+  if(imageSize.width < imageSize.height) {
+    newImage.width = newImage.side
+    newImage.height = null
+  } else {
+    newImage.height = newImage.side
+    newImage.width = null
+  }
 
-  newGmImageRessource = gmImageRessource.resize(400, 400)
+  newGmImageRessource = gmImageRessource.resize(newImage.width, newImage.height).crop(newImage.side, newImage.side, 0, 0)
 
   function syncToBuffer (gmRessource, callback) {
     gmRessource.toBuffer(callback)
