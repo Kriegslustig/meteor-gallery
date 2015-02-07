855892a8aece75cef60dd5ab464e3f1dcd641bdf
Kriegslustig <github@ls7.ch>
2015-02-07 20:38:13 +0100

    Add a close function the notifications

--- client/packages/notifiations/notifications.js
+++ client/packages/notifiations/notifications.js
@@ -1,12 +1,27 @@
 notification = {
   text: 'Notification'
 , type: 'message' // error || warning || message
+, config: {
+    notificationClass = 'notification'
+  , notificationRemoveClass = 'notification--removed'
+  , closeAnimationTime = 200 // ms
+  }
 , show: function () {
     var self = this
+  , allNotifications = []
   , data = {
       text: self.text
     , type: self.type
     }
     Blaze.renderWithData(Template.notification, data, document.body)
+    allNotifications = document.querySelectorAll('.' + self.config.notificationClass)
+    self.notificationElement = allNotifications[allNotifications.length - 1]
+  }
+, close: function () {
+    var self = this
+    self.notificationElement.addClass(self.config.notificationRemoveClass)
+    setTimeout(function () {
+      self.notificationElement.remove()
+    }, self.config.closeAnimationTime)
   }
 }
