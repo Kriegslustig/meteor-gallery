0df7621ca2003ffd81d38cc070915771d6417630
Kriegslustig <github@ls7.ch>
2015-02-07 20:54:53 +0100

    Auto close the button and provide an interface to close it

--- client/packages/notifiations/notifications.js
+++ client/packages/notifiations/notifications.js
@@ -1,10 +1,11 @@
 notification = {
   text: 'Notification'
 , type: 'message' // error || warning || message
+, timeToClose: 3000 // ms
 , config: {
-    notificationClass = 'notification'
-  , notificationRemoveClass = 'notification--removed'
-  , closeAnimationTime = 200 // ms
+    notificationClass: 'notification'
+  , notificationRemoveClass: 'notification--remove'
+  , closeAnimationTime: 200 // ms
   }
 , show: function () {
     var self = this
@@ -16,12 +17,22 @@ notification = {
     Blaze.renderWithData(Template.notification, data, document.body)
     allNotifications = document.querySelectorAll('.' + self.config.notificationClass)
     self.notificationElement = allNotifications[allNotifications.length - 1]
+    self.setEventListeners()
   }
 , close: function () {
     var self = this
-    self.notificationElement.addClass(self.config.notificationRemoveClass)
+    self.notificationElement.className += ' ' + self.config.notificationRemoveClass
     setTimeout(function () {
       self.notificationElement.remove()
     }, self.config.closeAnimationTime)
+  }
+, setEventListeners: function () {
+    var self = this
+    self.notificationElement.addEventListener('close', function () {
+      self.close()
+    })
+    setTimeout(function () {
+      self.close()
+    }, self.timeToClose)
   }
 }
